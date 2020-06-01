515fdace8b62dbf467d47469eb7e36b146fc3364
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-06-01 17:50:35 +0800

    .

--- crates/maple_cli/src/cmd/rpc/on_move.rs
+++ crates/maple_cli/src/cmd/rpc/on_move.rs
@@ -13,66 +13,6 @@ fn as_absolute_path<P: AsRef<Path>>(path: P) -> Result<String> {
         .map_err(|e| anyhow!("{:?}, path:{}", e, path.as_ref().display()))
 }
 
-fn apply_preview_file_at<P: AsRef<Path>>(
-    path: P,
-    lnum: usize,
-    size: usize,
-    msg_id: u64,
-    provider_id: &str,
-) {
-    match crate::utils::read_preview_lines(path.as_ref(), lnum, size) {
-        Ok((lines_iter, hi_lnum)) => {
-            let fname = format!("{}", path.as_ref().display());
-            let lines = std::iter::once(fname.clone())
-                .chain(lines_iter)
-                .collect::<Vec<_>>();
-            debug!("sending msg_id:{}, provider_id:{}", msg_id, provider_id);
-            write_response(
-                json!({ "id": msg_id, "provider_id": provider_id, "event": "on_move", "lines": lines, "fname": fname, "hi_lnum": hi_lnum }),
-            );
-        }
-        Err(err) => {
-            error!(
-                "[{}]Couldn't read first lines of {}, error: {:?}",
-                provider_id,
-                path.as_ref().display(),
-                err
-            );
-        }
-    }
-}
-
-fn apply_preview_file<P: AsRef<Path>>(
-    path: P,
-    size: usize,
-    msg_id: u64,
-    provider_id: &str,
-) -> Result<()> {
-    let abs_path = as_absolute_path(path.as_ref())?;
-    let lines_iter = crate::utils::read_first_lines(path.as_ref(), size)?;
-    let lines = std::iter::once(abs_path.clone())
-        .chain(lines_iter)
-        .collect::<Vec<_>>();
-    write_response(
-        json!({ "id": msg_id, "provider_id": provider_id, "event": "on_move", "lines": lines, "fname": abs_path }),
-    );
-    Ok(())
-}
-
-fn preview_directory<P: AsRef<Path>>(
-    path: P,
-    size: usize,
-    enable_icon: bool,
-    msg_id: u64,
-    provider_id: &str,
-) -> Result<()> {
-    let lines = super::filer::read_dir_entries(&path, enable_icon, Some(size))?;
-    write_response(
-        json!({ "id": msg_id, "provider_id": provider_id, "event": "on_move", "lines": lines, "is_dir": true }),
-    );
-    Ok(())
-}
-
 pub struct OnMoveHandler {
     pub msg_id: u64,
     pub provider_id: String,
@@ -97,35 +37,69 @@ impl From<Message> for OnMoveHandler {
 
 impl OnMoveHandler {
     pub fn handle(&self) -> Result<()> {
-        let preview_file =
-            |path: &Path| apply_preview_file(path, 2 * self.size, self.msg_id, &self.provider_id);
-
-        let preview_file_at = |path: &Path, lnum: usize| {
-            apply_preview_file_at(path, lnum, self.size, self.msg_id, &self.provider_id)
-        };
-
-        match self.which_move.clone() {
+        match &self.which_move {
             BLines { path, lnum }
             | Grep { path, lnum }
             | ProjTags { path, lnum }
             | BufferTags { path, lnum } => {
                 debug!("path:{}, lnum:{}", path.display(), lnum);
-                preview_file_at(&path, lnum);
+                self.preview_file_at(&path, *lnum);
             }
             Filer(path) if path.is_dir() => {
-                preview_directory(
-                    &path,
-                    2 * self.size,
-                    super::env::global().enable_icon,
-                    self.msg_id,
-                    "filer",
-                )?;
+                self.preview_directory(&path)?;
             }
             Files(path) | Filer(path) => {
-                preview_file(&path)?;
+                self.preview_file(&path)?;
             }
         }
 
+        Ok(())
+    }
+
+    fn preview_file_at<P: AsRef<Path>>(&self, path: P, lnum: usize) {
+        match crate::utils::read_preview_lines(path.as_ref(), lnum, self.size) {
+            Ok((lines_iter, hi_lnum)) => {
+                let fname = format!("{}", path.as_ref().display());
+                let lines = std::iter::once(fname.clone())
+                    .chain(lines_iter)
+                    .collect::<Vec<_>>();
+                debug!(
+                    "sending msg_id:{}, provider_id:{}",
+                    self.msg_id, self.provider_id
+                );
+                write_response(
+                    json!({ "id": self.msg_id, "provider_id": self.provider_id, "event": "on_move", "lines": lines, "fname": fname, "hi_lnum": hi_lnum }),
+                );
+            }
+            Err(err) => {
+                error!(
+                    "[{}]Couldn't read first lines of {}, error: {:?}",
+                    self.provider_id,
+                    path.as_ref().display(),
+                    err
+                );
+            }
+        }
+    }
+
+    fn preview_file<P: AsRef<Path>>(&self, path: P) -> Result<()> {
+        let abs_path = as_absolute_path(path.as_ref())?;
+        let lines_iter = crate::utils::read_first_lines(path.as_ref(), 2 * self.size)?;
+        let lines = std::iter::once(abs_path.clone())
+            .chain(lines_iter)
+            .collect::<Vec<_>>();
+        write_response(
+            json!({ "id": self.msg_id, "provider_id": self.provider_id, "event": "on_move", "lines": lines, "fname": abs_path }),
+        );
+        Ok(())
+    }
+
+    fn preview_directory<P: AsRef<Path>>(&self, path: P) -> Result<()> {
+        let enable_icon = super::env::global().enable_icon;
+        let lines = super::filer::read_dir_entries(&path, enable_icon, Some(2 * self.size))?;
+        write_response(
+            json!({ "id": self.msg_id, "provider_id": self.provider_id, "event": "on_move", "lines": lines, "is_dir": true }),
+        );
         Ok(())
     }
 }
